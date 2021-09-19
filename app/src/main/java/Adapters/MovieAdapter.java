18d1409b80ca67166fa033e67aa149b63fde231c
tobur3 <tnburto96@yahoo.com>
2021-09-19 19:00:17 -0400

    Update for part 2

--- app/src/main/java/Adapters/MovieAdapter.java
+++ app/src/main/java/Adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 
 import android.util.Log;
@@ -8,13 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.R;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -58,6 +64,7 @@ public class MovieAdapter  extends RecyclerView.Adapter<MovieAdapter.Viewholder>
 
 
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -67,6 +74,7 @@ public class MovieAdapter  extends RecyclerView.Adapter<MovieAdapter.Viewholder>
             tvTitle= itemView.findViewById(R.id.tvTitle);
             tvOverview= itemView.findViewById(R.id.tvOverview);
             ivPoster= itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
@@ -82,6 +90,16 @@ public class MovieAdapter  extends RecyclerView.Adapter<MovieAdapter.Viewholder>
                 imageUrl = movie.getPosterPath();
             }
             Glide.with(context).load(imageUrl).into(ivPoster);
+            //1. register click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //2. navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
